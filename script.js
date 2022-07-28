9a076911dea852dcbe3b24753def85be593646dd
mohammedsohail-hacker <79409236+mohammedsohail-hacker@users.noreply.github.com>
2022-07-28 17:49:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Programmer", "Blogger", "Sportsman","Dancer"],
+        strings: ["Student", "Programmer", "Sportsman","Dancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Programmer", "Blogger", "Sportsman","Dancer"],
+        strings: ["Student", "Programmer", "Sportsman","Dancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
